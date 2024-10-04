9611b35a1412bbc27b545c87c7b284e6b83d09d3
aktm <aktm@yandex.ru>
2024-10-04 22:02:52 +0700

    Add date component

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,25 @@
 import { defineComponent, createApp } from 'vue'
+
+const App = defineComponent({
+  name: 'DateApp',
+
+  setup() {
+    const options = {
+      dateStyle: 'long',
+    }
+
+    const dateFormatted = new Date().toLocaleDateString('ru-RU', options)
+
+    return {
+      dateFormatted,
+    }
+  },
+
+  template: `
+          <h1>Сегодня {{ dateFormatted }}</h1>
+    `,
+})
+
+const app = createApp(App)
+
+app.mount('#app')
