90afd88ba5a00abf3cb272c31f1c0eaacdd407b5
aktm <aktm@yandex.ru>
2024-10-29 21:10:17 +0700

    Add calculator

--- 02-basics-2/30-calculator/CalculatorApp.js
+++ 02-basics-2/30-calculator/CalculatorApp.js
@@ -1,26 +1,56 @@
-import { defineComponent } from 'vue'
+import { ref, computed, defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'CalculatorApp',
 
-  setup() {},
+  setup() {
+    const x = ref(0)
+    const y = ref(0)
+    const operator = ref('sum')
+
+    const result = computed(() => {
+      const val1 = x.value
+      const val2 = y.value
+      const operatorValue = operator.value
+
+      switch (operatorValue) {
+        case 'sum':
+          return val1 + val2
+        case 'subtract':
+          return val1 - val2
+        case 'multiply':
+          return val1 * val2
+        case 'divide':
+          return val1 / val2
+        default:
+          return 0
+      }
+    })
+
+    return {
+      x,
+      y,
+      operator,
+      result,
+    }
+  },
 
   template: `
     <div class="calculator">
-      <input type="number" aria-label="First operand" />
+      <input type="number" aria-label="First operand" v-model="x"/>
 
       <div class="calculator__operators">
-        <label><input type="radio" name="operator" value="sum"/>➕</label>
-        <label><input type="radio" name="operator" value="subtract"/>➖</label>
-        <label><input type="radio" name="operator" value="multiply"/>✖</label>
-        <label><input type="radio" name="operator" value="divide"/>➗</label>
+        <label><input type="radio" name="operator" value="sum" v-model="operator"/>➕</label>
+        <label><input type="radio" name="operator" value="subtract" v-model="operator"/>➖</label>
+        <label><input type="radio" name="operator" value="multiply" v-model="operator"/>✖</label>
+        <label><input type="radio" name="operator" value="divide" v-model="operator"/>➗</label>
       </div>
 
-      <input type="number" aria-label="Second operand" />
+      <input type="number" aria-label="Second operand" v-model="y"/>
 
       <div>=</div>
 
-      <output>0</output>
+      <output>{{ result }}</output>
     </div>
   `,
 })
